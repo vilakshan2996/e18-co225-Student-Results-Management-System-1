c339ab379a030cc3e0654e85e1cef9bca06de949
unknown <vilakshan2996@gmail.com>
2022-02-26 20:35:41 +0530

    Android Project Setup With Firebase

---
+++ SRMS/app/src/main/java/com/sharetechidea/srms/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sharetechidea.srms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
